0329b775d302bb1cec1a9455d5650db1a039dd71
Petr Pavlis <petrpavlis123@gmail.com>
2024-10-06 23:25:49 +0200

    První commit

---
+++ View/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MonkeyFinder.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent(); // Inicializuje komponenty z XAML
+        }
+
+        // Můžeš přidat další metody a logiku, pokud je potřeba
+    }
+}
